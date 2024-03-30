4c509687c1ada9a890848db0d59f1fa9d2842ac5
nzontsika <noel.zontsika@yahoo.fr>
2024-03-30 13:17:20 +0100

    Création des composants du menu

---
+++ SION/SION-UI/SIONPROJECT/src/app/menu/a-propos/les-offices/les-offices.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-les-offices',
+  standalone: true,
+  imports: [],
+  templateUrl: './les-offices.component.html',
+  styleUrl: './les-offices.component.css'
+})
+export class LesOfficesComponent {
+
+}
---
+++ SION/SION-UI/SIONPROJECT/src/app/menu/services/nous-rejoindre/nous-rejoindre.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nous-rejoindre',
+  standalone: true,
+  imports: [],
+  templateUrl: './nous-rejoindre.component.html',
+  styleUrl: './nous-rejoindre.component.css'
+})
+export class NousRejoindreComponent {
+
+}
