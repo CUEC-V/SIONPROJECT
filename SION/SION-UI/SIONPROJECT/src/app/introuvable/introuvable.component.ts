7bfd335931b6f43958cf96e2388d126c5ab30208
nzontsika <noel.zontsika@yahoo.fr>
2024-04-02 11:47:24 +0200

    Routing du menu

---
+++ SION/SION-UI/SIONPROJECT/src/app/introuvable/introuvable.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-introuvable',
+  standalone: true,
+  imports: [],
+  templateUrl: './introuvable.component.html',
+  styleUrl: './introuvable.component.css'
+})
+export class IntrouvableComponent {
+
+}
