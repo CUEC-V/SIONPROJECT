41dd84b86782ef8c0da9cff6bcbcaa27fefa8b67
arielle243 <nionguinzaba.arielle@gmail.com>
2024-06-08 14:44:53 +0200

    modification page ressources-annonces

---
+++ SION/SION-UI/SIONPROJECT/src/app/menu/ressources/pages/baptemes/baptemes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-baptemes',
+  standalone: true,
+  imports: [],
+  templateUrl: './baptemes.component.html',
+  styleUrl: './baptemes.component.css'
+})
+export class BaptemesComponent {
+
+}
