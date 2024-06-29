735ead64c99de63ed42e82508525dda350ca9fa5
nzontsika <noel.zontsika@yahoo.fr>
2024-06-29 12:55:54 +0200

    Fichier de configuration : yt_key_pourvu

--- SION/SION-UI/SIONPROJECT/src/app/configuration.ts
+++ SION/SION-UI/SIONPROJECT/src/app/configuration.ts
@@ -1,8 +1,8 @@
 export class Configuration {
     static readonly UrlApi: string = 'https://localhost:7213/api';
 
-    static readonly Youtube_API_KEY: string = '';
-    static readonly Youtube_CHANNEL_NAME = '';
+    static readonly Youtube_API_KEY: string = '';// Valeurs non pourvues
+    static readonly Youtube_CHANNEL_NAME = '';// Valeurs non pourvues
     static readonly Youtube_VIDEOS_NUMBER = 12;
     static readonly Youtube_VIDEO_PASTEUR = 'AR2FNCP2LL8';
     static readonly PAT_DATE_CREATION = '2004';
